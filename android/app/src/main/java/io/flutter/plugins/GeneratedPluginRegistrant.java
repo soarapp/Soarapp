156c75a5f80bfa95fce170ca5027e131d2072b8a
Tarun Gunampalli <tarun.gunampalli@gmail.com>
2021-03-19 14:27:15 -0500

    Added header and basic list

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -4,7 +4,6 @@ import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistry;
 
 /**
  * Generated file. Do not edit.
@@ -14,7 +13,5 @@ import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistry;
 @Keep
 public final class GeneratedPluginRegistrant {
   public static void registerWith(@NonNull FlutterEngine flutterEngine) {
-    ShimPluginRegistry shimPluginRegistry = new ShimPluginRegistry(flutterEngine);
-      de.minimalme.spotify_sdk.SpotifySdkPlugin.registerWith(shimPluginRegistry.registrarFor("de.minimalme.spotify_sdk.SpotifySdkPlugin"));
   }
 }
--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -4,16 +4,9 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<spotify_sdk/SpotifySdkPlugin.h>)
-#import <spotify_sdk/SpotifySdkPlugin.h>
-#else
-@import spotify_sdk;
-#endif
-
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [SpotifySdkPlugin registerWithRegistrar:[registry registrarForPlugin:@"SpotifySdkPlugin"]];
 }
 
 @end
