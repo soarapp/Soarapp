eb46488c5aaa6643665528f2579b605259724498
Tarun Gunampalli <tarun.gunampalli@gmail.com>
2021-06-01 10:56:36 -0500

    removed extra logger line

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -4,12 +4,16 @@
 
 // ignore_for_file: lines_longer_than_80_chars
 
+import 'package:audioplayers/audioplayers_web.dart';
 import 'package:spotify_sdk/spotify_sdk_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
+  AudioplayersPlugin.registerWith(registrar);
   SpotifySdkPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
